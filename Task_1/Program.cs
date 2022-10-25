6cae47cbad6064b9cafb736549be29fad7277dd2
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-25 22:59:35 +0500

    Приём с консоли чисел и определение количества положительных чисел

---
+++ Task_1/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 41:
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+int [] GetArrayFromConsole()
+{
+    Console.WriteLine("Введите числа через запятую:");
+    int [] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+    return array;
+}
+int GetQuantityPositiveNumber (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count ++;
+    }
+    return count;
+}
+void PrintArray(int [] array)
+{   
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+int[] array = GetArrayFromConsole();
+int positiveNumb = GetQuantityPositiveNumber(array);
+PrintArray(array);
+Console.Write($"-> Введено {positiveNumb} чисел больше 0");
+
+
